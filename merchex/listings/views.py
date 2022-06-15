b3289fdfaae33858de86c33c1aa873cec88a0025
fbastier <francois.bastier@gmail.com>
2022-06-15 17:16:12 +0200

    Partie 2 - 4

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,27 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from listings.models import Band
 
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes :</p>
-        <ul>
-            <li>{bands[0].name} : {bands[0].title}</li>
-            <li>{bands[1].name} : {bands[1].title}</li>
-            <li>{bands[2].name} : {bands[2].title}</li>
-        </ul>
-""")
+    return render(request, 'listings/hello.html', context={'bands':bands})
 
 
 def about(request):
-    return HttpResponse('<h1>A propos</h1><br><p>Nous sommes merchex</p>')
+    return render(request, 'listings/about.html')
 
 
 def listings(request):
-    return HttpResponse('<h1>Liste des bidules</h1>')
+    titles = Band.objects.all()
+    return render(request, 'listings/listings.html', context={'titles':titles})
 
 
 def contact(request):
-    return HttpResponse('<h1>Formulaire de contact</h1>')
+    return render(request, 'listings/contact.html')
